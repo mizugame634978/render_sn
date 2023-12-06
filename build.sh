699fd81115d895423e86791b3b086544f75d12e5
mizu <ryouta1853@gmail.com>
2023-12-07 03:44:37 +0900

    a

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py superuser
+                 
 python manage.py chara_init
--- smash_proj/management/commands/superuser.py
+++ smash_proj/management/commands/superuser.py
@@ -3,12 +3,14 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 
 User = get_user_model()
+
+
 class Command(BaseCommand):
   def handle(self, *args, **options):
     if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
-        User.objects.create_superuser(
-            username=settings.SUPERUSER_NAME,
-            email=settings.SUPERUSER_EMAIL,
-            password=settings.SUPERUSER_PASSWORD
-        )
-        print("スーパーユーザー作成")
+      User.objects.create_superuser(
+          username=settings.SUPERUSER_NAME,
+          email=settings.SUPERUSER_EMAIL,
+          password=settings.SUPERUSER_PASSWORD
+      )
+      print("スーパーユーザー作成")
