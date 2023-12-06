6bb3c0715d0a5766808c3b896af9f649f765cde6
mizu <ryouta1853@gmail.com>
2023-12-07 03:40:17 +0900

    sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python mangage.py migrate
-python mangage.py superuser
-python mangage.py chara_init
+python manage.py migrate
+python manage.py superuser
+python manage.py chara_init
