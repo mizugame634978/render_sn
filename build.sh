09a6143ae5e2e6c1744fc29b1999a0004f3da2d6
mizu <ryouta1853@gmail.com>
2023-12-14 15:34:01 +0900

    matchのラジオ

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations
-echo "make"
 python manage.py migrate
-echo "migrate"
-python manage.py superuser
+# python manage.py superuser
 # python manage.py character_init
--- smash_proj/settings.py
+++ smash_proj/settings.py
@@ -14,10 +14,6 @@ env.read_env(os.path.join(BASE_DIR,'.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
